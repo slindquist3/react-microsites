b3faf4e0c32ca721d45c37298ccf5e8da63e9eb6
Scott Lindquist <scott.lindquist@equinox.com>
2018-10-23 14:54:01 -0400

    integrate fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+const italic = {fontStyle: "italic"};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <p>This is a template for Furthermore microsites and landing pages.</p>
+          <p>This is a template for <span style={italic}>Furthermore</span> microsites and landing pages.</p>
       </div>
     );
   }
