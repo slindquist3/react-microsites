34aca8ef0119f57f2ec4e6b823ee0ed34f7e68d2
Scott Lindquist <scott.lindquist@equinox.com>
2018-10-23 17:38:49 -0400

    change copy

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.scss';
 
 class App extends React.Component {
   render() {
+
+    const style = {fontStyle: "italic"}
+
     return (
       <div className="App">
-        <h1>Home</h1>
+        <h1>This is a template for <span style={style}>Furthermore</span> microsites</h1>
       </div>
     )
   }
