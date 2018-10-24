29712076ba06604e6bc5aad744153a75c7829918
Scott Lindquist <scott.lindquist@equinox.com>
2018-10-24 11:11:14 -0400

    update project structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
+import App from './components/App'
 //change below components to suit your navigation
 import View1 from './components/view1'
 import View2 from './components/view2'
+import './index.scss';
 
 const routing = (
   <Router>
